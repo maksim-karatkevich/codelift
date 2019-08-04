c56e4821a1b689f287bf72c0f72842cb8d7672bb
Eric Clemmons <eric@smarterspam.com>
2019-08-04 15:18:28 -0700

    Add custom type for unique-selector

---
+++ src/types/unique-selector.d.ts
@@ -0,0 +1,16 @@
+// https://github.com/ericclemmons/unique-selector/issues/37
+declare module "unique-selector" {
+  export type Selector = string;
+
+  export type SelectorType = "ID" | "Class" | "Tag" | "NthChild" | "Attributes";
+
+  export type Options = {
+    selectorTypes?: SelectorType[];
+    attributesToIgnore?: string[];
+    excludeRegex?: RegExp | null;
+  };
+
+  declare function unique(element: Element, options?: Options): Selector;
+
+  export = unique;
+}
