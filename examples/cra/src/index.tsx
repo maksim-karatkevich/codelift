866eff2e890a45d2e0f3f93c18bef600fef070cf
Eric Clemmons <eric@smarterspam.com>
2019-08-25 12:01:43 -0700

    Move GUI from examples/cra to packages/gui

--- examples/cra/src/index.tsx
+++ examples/cra/src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { App } from "./components/App";
+import "tailwindcss/dist/tailwind.css";
+
 import * as serviceWorker from "./serviceWorker";
 
 // Allow access from GUI on another port
@@ -10,7 +11,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 ReactDOM.render(
-  <App />,
+  <span />,
   document.body.appendChild(document.createElement("div"))
 );
 
