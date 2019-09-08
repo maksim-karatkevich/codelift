6e13b7f6acfca4766dd23a5529980affe5c1401f
Eric Clemmons <eric@smarterspam.com>
2019-09-08 16:52:38 -0700

    Open localhost:1337

--- packages/codevisor/server.js
+++ packages/codevisor/server.js
@@ -1,5 +1,6 @@
 const { createServer } = require("http");
 const next = require("next");
+const open = require("open");
 
 const dev = process.env.NODE_ENV !== "production";
 const dir = __dirname;
@@ -10,7 +11,10 @@ const app = next({ dev, dir });
 createServer(app.getRequestHandler()).listen(PORT, err => {
   if (err) throw err;
 
-  console.log(`> codevisor started on http://localhost:${PORT}`);
+  const url = `http://localhost:${PORT}`;
+
+  console.log(`> codevisor started on ${url}`);
+  open(url);
 
   app.prepare();
 });
