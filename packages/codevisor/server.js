315867f5fc63a36f35e2858f349b6931218842d4
Eric Clemmons <eric@smarterspam.com>
2019-11-06 20:06:25 -0800

    server redirects non-/api requests to /

--- packages/codevisor/server.js
+++ packages/codevisor/server.js
@@ -1,20 +1,37 @@
 const { createServer } = require("http");
 const next = require("next");
 const open = require("open");
+const { parse } = require("url");
 
 const dev = process.env.NODE_ENV !== "production";
 const dir = __dirname;
 const { PORT = 1337 } = process.env;
 
 const app = next({ dev, dir });
+const handle = app.getRequestHandler();
 
-createServer(app.getRequestHandler()).listen(PORT, err => {
-  if (err) throw err;
+app
+  .prepare()
+  .then(() => {
+    createServer((req, res) => {
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
 
-  const url = `http://localhost:${PORT}`;
+      if (pathname === "/api") {
+        handle(req, res);
+      } else {
+        app.render(req, res, "/", query);
+      }
+    }).listen(PORT, err => {
+      if (err) throw err;
 
-  console.log(`> codevisor started on ${url}`);
-  open(url);
+      const url = `http://localhost:${PORT}`;
 
-  app.prepare();
-});
+      console.log(`> codevisor started on ${url}`);
+      open(url);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
