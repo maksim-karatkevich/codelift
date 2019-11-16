65aa00fe2c053b6fc60e1ef07b4a1b20f7ba44f3
Eric Clemmons <eric@smarterspam.com>
2019-11-16 15:43:03 -0800

    Giving up

--- packages/codelift/components/App/index.tsx
+++ packages/codelift/components/App/index.tsx
@@ -19,7 +19,7 @@ export const App: FunctionComponent = observer(() => {
 
   return (
     <Provider value={client}>
-      <Sidebar>
+      {/* <Sidebar>
         {store.root ? (
           <>
             <Panel label="CSS">
@@ -47,23 +47,55 @@ export const App: FunctionComponent = observer(() => {
         ) : (
           <p className="text-white text-center italic">Loading&hellip;</p>
         )}
-      </Sidebar>
+      </Sidebar> */}
 
       {store.isOpen && <Selector />}
 
-      <main
-        className="h-screen relative bg-white"
+      <section
+        className="bg-gray-100"
         style={{
-          boxShadow: "0 0 10px rgba(0, 0, 0, 0.15)"
+          display: "grid",
+          gridTemplateColumns: "16rem 1fr 16rem"
         }}
       >
-        <iframe
-          onLoad={store.handleFrameLoad}
-          className="w-full h-full shadow-lg"
-          src={`http://localhost:3000${path}`}
-          title="Source"
-        />
-      </main>
+        <aside>
+          <h1
+            className="px-4 py-4 text-xl font-serif tracking-wider text-teal-700 font-thin"
+            style={{
+              textShadow: "0 1px 0 #FFFFFF, 0 -1px 0 #00000020"
+            }}
+          >
+            code<em className="ml-px opacity-75">lift</em>
+          </h1>
+
+          <form className="flex items-center bg-transparent border border-gray-400 bg-white focus:shadow-outline rounded-lg px-2 py-1">
+            <input
+              className="appearance-none w-full"
+              placeholder="Filter..."
+              type="search"
+            />
+            <button>Howdy</button>
+          </form>
+        </aside>
+
+        <section className="pt-4 h-screen">
+          <main
+            className="h-full relative bg-white rounded-t overflow-hidden"
+            style={{
+              boxShadow: "0 0 10px rgba(0, 0, 0, 0.15)"
+            }}
+          >
+            <iframe
+              onLoad={store.handleFrameLoad}
+              className="w-full h-full shadow-lg"
+              src={`http://localhost:3000${path}`}
+              title="Source"
+            />
+          </main>
+        </section>
+
+        <aside>Panels</aside>
+      </section>
     </Provider>
   );
 });
