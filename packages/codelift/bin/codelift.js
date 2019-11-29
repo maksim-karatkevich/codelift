8ca8499d64cd2bd0fdce4edfb844f243a588b890
Eric Clemmons <eric@smarterspam.com>
2019-11-28 21:50:29 -0800

    Spawn child after server is required

--- packages/codelift/bin/codelift.js
+++ packages/codelift/bin/codelift.js
@@ -6,9 +6,9 @@ const [, , ...args] = process.argv;
 // TODO Use npm if package-lock.json exists
 const command = "yarn";
 
+require("../server");
+
 // Start subprocess
 spawn(command, args.length ? args : ["start"], {
   stdio: "inherit"
 });
-
-require("../server");
