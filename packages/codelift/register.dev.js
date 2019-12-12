41a70ffae45e31ce630c70eb9b15430a05a4489d
Eric Clemmons <eric@smarterspam.com>
2019-12-11 20:03:36 -0800

    Only access window.__CODELIFT__ if it's been set

--- packages/codelift/register.dev.js
+++ packages/codelift/register.dev.js
@@ -3,6 +3,10 @@ document.domain = window.location.hostname;
 
 if (module.hot) {
   module.hot.addStatusHandler(status => {
-    window["__CODELIFT__"].handleStatus(status);
+    const store = window["__CODELIFT__"];
+
+    if (store) {
+      store.handleStatus(status);
+    }
   });
 }
