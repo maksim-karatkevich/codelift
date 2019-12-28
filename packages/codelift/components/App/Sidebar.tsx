a320138e4a0bf21cbf52c40691522f2d36d804a9
Eric Clemmons <eric@smarterspam.com>
2019-12-28 11:56:45 -0600

    Use Tailwind for the Sidebar

--- packages/codelift/components/App/Sidebar.tsx
+++ packages/codelift/components/App/Sidebar.tsx
@@ -1,17 +1,7 @@
-import { Box } from "@chakra-ui/core";
 import { FunctionComponent } from "react";
 
 export const Sidebar: FunctionComponent = ({ children }) => (
-  <Box
-    as="aside"
-    bg="gray.700"
-    display="flex"
-    flexDirection="column"
-    justifyContent="start"
-    height="100vh"
-    overflow="auto"
-    shadow="inner"
-  >
+  <aside className="bg-gray-900 flex flex-col justify-start h-screen overflow-auto shadow-inner">
     {children}
-  </Box>
+  </aside>
 );
