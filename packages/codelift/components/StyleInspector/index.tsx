e2d27681a825a6b431f147ea14d2ab9851211a78
Eric Clemmons <eric@smarterspam.com>
2020-01-13 16:31:11 -0800

    Sticky headers

--- packages/codelift/components/StyleInspector/index.tsx
+++ packages/codelift/components/StyleInspector/index.tsx
@@ -7,7 +7,7 @@ import { SliderMenu } from "./SliderMenu";
 export const StyleInspector: FunctionComponent = () => {
   // TODO Add a toggle for :hover,, :focus, :active based on selectorText
   const Heading: FunctionComponent = ({ children }) => (
-    <label className="flex items-center text-left text-white bg-black px-2 py-1 shadow text-sm w-full">
+    <label className="flex items-center text-left text-white bg-black px-2 py-1 shadow sticky text-sm top-0 w-full z-10">
       {children}
     </label>
   );
