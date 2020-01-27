c0d42596d7df6047acacd746e268f89df5daefc5
Eric Clemmons <eric@smarterspam.com>
2020-01-26 16:43:31 -0800

    Only render iframe with initial path

--- packages/codelift/components/App/index.tsx
+++ packages/codelift/components/App/index.tsx
@@ -6,7 +6,8 @@ import {
   Grid,
   useToast
 } from "@chakra-ui/core";
-import React, { FunctionComponent, useEffect } from "react";
+import { useRouter } from "next/router";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { GitHub, HelpCircle } from "react-feather";
 import { createClient, Provider } from "urql";
 
@@ -21,9 +22,10 @@ const client = createClient({ url: "/api" });
 
 export const App: FunctionComponent = observer(() => {
   const store = useStore();
+  const router = useRouter();
+  // Only render the initial path to prevent double-reloads.
+  const [initialPath] = useState(store.path);
   const toast = useToast();
-  const { href, origin } = window.location;
-  const path = href.split(origin).pop();
 
   useEffect(() => {
     if (store.state === "HIDDEN") {
@@ -101,7 +103,7 @@ export const App: FunctionComponent = observer(() => {
         <Box as="main" boxShadow="lg" height="100vh" overflow="auto" zIndex={1}>
           <iframe
             onLoad={store.handleFrameLoad}
-            src={`http://localhost:3000${path}`}
+            src={`http://localhost:3000${initialPath}`}
             style={{ width: "100%", height: "100%" }}
             title="Source"
           />
