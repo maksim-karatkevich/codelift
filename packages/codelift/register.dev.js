354ea938d1da828bf85e73f5593a7e00b079ef0d
Eric Clemmons <eric@smarterspam.com>
2020-01-26 16:43:19 -0800

    syncPath on history pushState|replaceState|popState

--- packages/codelift/register.dev.js
+++ packages/codelift/register.dev.js
@@ -1,6 +1,28 @@
 // Allow access from GUI on another port
 document.domain = window.location.hostname;
 
+const { pushState, replaceState } = window.history;
+
+const syncPath = () => {
+  const store = window["__CODELIFT__"];
+
+  if (store) {
+    setTimeout(store.syncPath, 0);
+  }
+};
+
+window.history.pushState = (...args) => {
+  pushState.apply(window.history, args);
+  syncPath();
+};
+
+window.history.replaceState = (...args) => {
+  replaceState.apply(window.history, args);
+  syncPath();
+};
+
+window.addEventListener("popstate", syncPath);
+
 if (module.hot) {
   module.hot.addStatusHandler(status => {
     const store = window["__CODELIFT__"];
