d5d7ca13ea5115c51adcaf5c3255318c756473ac
Eric Clemmons <eric@smarterspam.com>
2020-01-26 20:53:42 -0800

    Use single store instance

--- packages/codelift/store.ts
+++ packages/codelift/store.ts
@@ -3,6 +3,8 @@ import { observer } from "mobx-react-lite";
 
 import { App } from "./models/App";
 
+const store = App.create();
+
 export { observer };
-export const StoreContext = createContext(App.create());
+export const StoreContext = createContext(store);
 export const useStore = () => useContext(StoreContext);
