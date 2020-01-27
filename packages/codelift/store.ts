813948093d2fd802862edda1b36e40c41f582932
Eric Clemmons <eric@smarterspam.com>
2020-01-26 20:54:45 -0800

    store supports RPC calls via postMessage

--- packages/codelift/store.ts
+++ packages/codelift/store.ts
@@ -5,6 +5,25 @@ import { App } from "./models/App";
 
 const store = App.create();
 
+window.addEventListener("message", event => {
+  const { source, payload } = event.data;
+
+  if (source !== "codelift") {
+    return;
+  }
+
+  const { action, args } = payload;
+
+  if (action in store) {
+    // @ts-ignore
+    store[action].apply(store, args);
+  } else {
+    throw new Error(
+      `codelift does not support action ${JSON.stringify(action)}`
+    );
+  }
+});
+
 export { observer };
 export const StoreContext = createContext(store);
 export const useStore = () => useContext(StoreContext);
