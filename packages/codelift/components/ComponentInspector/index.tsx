428351ebb088e7ba923f1856f1752afb60ef311c
Eric Clemmons <eric@smarterspam.com>
2020-02-22 12:31:54 -0800

    Working Component preview

--- packages/codelift/components/ComponentInspector/index.tsx
+++ packages/codelift/components/ComponentInspector/index.tsx
@@ -1,15 +1,41 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useStore, observer } from "../../store";
 
 export const ComponentInspector = observer(() => {
+  const ref = useRef(null);
   const store = useStore();
-  const Inspector = store.selected?.instance.type.Inspector;
+  const type = store.selected?.instance.type;
+  const Inspector = type.Inspector;
+  const [props, setProps] = useState({
+    ...store.selected?.instance.memoizedProps
+  });
 
-  if (Inspector) {
-    // ! This is britle dependency injection, as any hooks outside of
-    // ! the Inspector's scope (e.g. `useQuery`) won't share this React instance.
-    return <Inspector useState={useState} />;
+  useEffect(() => {
+    if (!ref.current || !Inspector) {
+      return;
+    }
+
+    // Use Component's copy of React & ReactDOM so that hooks work
+    Inspector.ReactDOM.render(
+      Inspector.React.createElement(Inspector, { props, setProps }),
+      ref.current
+    );
+  }, [ref.current, props, Inspector]);
+
+  useEffect(() => {
+    store.selected?.previewProps(props);
+  }, [props]);
+
+  if (!Inspector && store.selected?.isUserCode) {
+    return (
+      <button
+        className="self-center border px-2 bg-green-500 border-green-300 shadow py-px hover:bg-green-400"
+        onClick={() => store.selected?.openInIDE()}
+      >
+        Open in IDE
+      </button>
+    );
   }
 
-  return <div>No Inspector</div>;
+  return <section ref={ref} />;
 });
