1592b67fcae55d76f07980aaaaadad80cc35ba5f
Eric Clemmons <eric@smarterspam.com>
2020-03-01 21:15:27 -0800

    Working updateProps

--- packages/codelift/components/Header/index.tsx
+++ packages/codelift/components/Header/index.tsx
@@ -1,11 +1,25 @@
 import { FunctionComponent } from "react";
 import { Check, GitHub, HelpCircle } from "react-feather";
 import { observer, useStore } from "../../store";
+import { useMutation } from "urql";
 
 const Spacer = () => <div className="flex-grow" />;
 
 export const Header: FunctionComponent = observer(() => {
   const store = useStore();
+  const [res, updateProps] = useMutation(`
+    mutation UpdateProps(
+      $props: JSONObject!
+      $fileName: String!
+      $lineNumber: Int!
+    ) {
+      updateProps(
+        props: $props
+        fileName: $fileName
+        lineNumber: $lineNumber
+      )
+    }
+  `);
 
   return (
     <header
@@ -51,7 +65,26 @@ export const Header: FunctionComponent = observer(() => {
 
           <div className="flex flex-col shadow-md rounded overflow-hidden">
             <div className="flex flex-wrap">
-              <button className="flex items-center relative p-1 px-2 bg-gray-200 font-thin text-xs text-gray-700 border-gray-400 border-b hover:bg-white active:bg-green-200 active:shadow-inner">
+              <button
+                className="flex items-center relative p-1 px-2 bg-gray-200 font-thin text-xs text-gray-700 border-gray-400 border-b hover:bg-white active:bg-green-200 active:shadow-inner"
+                onClick={() => {
+                  if (store.selected) {
+                    // Since `undefined` isn't valid JSON, all values are serialized to JSON
+                    const serializedProps = Object.entries(
+                      store.selected.props
+                    ).reduce((acc, [prop, value]) => {
+                      acc[prop] = `${JSON.stringify(value)}`;
+                      return acc;
+                    }, {} as Record<string, string>);
+
+                    updateProps({
+                      props: serializedProps,
+                      fileName: store.selected.fileName,
+                      lineNumber: store.selected.lineNumber
+                    });
+                  }
+                }}
+              >
                 <Check className="mr-1" size={13} /> Save Changes
               </button>
             </div>
--- packages/codelift/schema/typeDefs/index.ts
+++ packages/codelift/schema/typeDefs/index.ts
@@ -54,6 +54,24 @@ export const typeDefs = gql`
       """
       lineNumber: Int!
     ): String
+
+    """
+    Add, replace, or remove props on a Component
+    """
+    updateProps(
+      """
+      Props to update (undefined to remove)
+      """
+      props: JSONObject!
+      """
+      element._reactInstance._debugSource.fileName
+      """
+      fileName: String!
+      """
+      element._reactInstance._debugSource.lineNumber
+      """
+      lineNumber: Int!
+    ): JSONObject
   }
 
   type Query {
