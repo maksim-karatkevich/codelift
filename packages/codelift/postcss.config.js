d2591836d2d7379820f69f5f226e3ae816492441
Eric Clemmons <eric@smarterspam.com>
2020-04-05 18:47:37 -0700

    Update postcss.config.js

--- packages/codelift/postcss.config.js
+++ packages/codelift/postcss.config.js
@@ -1,17 +1,3 @@
-const path = require("path");
-
 module.exports = {
-  plugins: [
-    require("postcss-easy-import"),
-    require("tailwindcss")(path.resolve(__dirname, "tailwind.config.js")),
-    require("@fullhuman/postcss-purgecss")({
-      content: [
-        "./pages/**/*.{js,jsx,ts,tsx}",
-        "./components/**/*.{js,jsx,ts,tsx}"
-      ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    }),
-    require("autoprefixer"),
-    require("cssnano")
-  ]
+  plugins: ["tailwindcss", "postcss-preset-env"],
 };
