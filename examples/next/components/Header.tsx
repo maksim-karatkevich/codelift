70cfc8f138ba7e10884dc76511228a7480bb410d
Eric Clemmons <eric@smarterspam.com>
2020-04-06 23:26:00 -0700

    Convert from Header.Inspector to useInspector

--- examples/next/components/Header.tsx
+++ examples/next/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useInspector } from "codelift";
 import { useRouter } from "next/router";
 import { FunctionComponent } from "react";
 
@@ -5,14 +6,32 @@ const greetings = ["hello", "saluton", "hei", "bonjour", "guten tag", "aloha"];
 
 type HeaderProps = { title: string };
 
-export const Header: FunctionComponent<HeaderProps> = ({ title }) => {
+const HeaderInspector = ({ props, setProps }) => {
+  return (
+    <label className="flex flex-col px-4">
+      <small className="opacity-75 italic tracking-wider">Title</small>
+      <input
+        autoFocus
+        className="bg-transparent border-b border-dotted"
+        onChange={(event) => setProps({ title: event.target.value })}
+        defaultValue={props.title}
+      />
+    </label>
+  );
+};
+
+export const Header: FunctionComponent<HeaderProps> = (props) => {
+  const [ref, { title }] = useInspector(props, HeaderInspector);
   const router = useRouter();
   const { path = [] } = router.query;
   const [greeting] = path as string[];
   const next = greetings[(greetings.indexOf(greeting) + 1) % greetings.length];
 
   return (
-    <div className="bg-black text-white text-center text-5xl shadow-lg py-12">
+    <div
+      className="bg-black text-white text-center text-5xl shadow-lg py-12"
+      ref={ref}
+    >
       <h1 className="tracking-wider">
         <a href={`/${next}`}>
           {greeting && (
@@ -22,19 +41,5 @@ export const Header: FunctionComponent<HeaderProps> = ({ title }) => {
         </a>
       </h1>
     </div>
-  );
-};
-
-Header["Inspector"] = ({ props, setProps }) => {
-  return (
-    <label className="flex flex-col px-4">
-      <small className="opacity-75 italic tracking-wider">Title</small>
-      <input
-        autoFocus
-        className="bg-transparent border-b border-dotted"
-        onChange={event => setProps({ title: event.target.value })}
-        value={props.title}
-      />
-    </label>
   );
 };
